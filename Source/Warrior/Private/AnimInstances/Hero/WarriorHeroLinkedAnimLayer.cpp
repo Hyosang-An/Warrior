e88bb1a698623a3f2a2002749a60ac009707f6b1
Hyosang_An <gytharj@gmail.com>
2025-05-06 21:40:53 +0900

    Adds axe weapon support and enhances animation systems
    Introduces new assets and logic for axe weapon, including animations, blend spaces, and gameplay abilities. Implements utility functions for weapon retrieval and updates data structures to support hero weapon data. Enhances animation layer linking and improves modularity.
    
    Fixes function name mismatch in core redirects.

--- Source/Warrior/Private/AnimInstances/Hero/WarriorHeroLinkedAnimLayer.cpp
+++ Source/Warrior/Private/AnimInstances/Hero/WarriorHeroLinkedAnimLayer.cpp
@@ -1,5 +1,9 @@
-
-
-
 #include "AnimInstances/Hero/WarriorHeroLinkedAnimLayer.h"
 
+#include "AnimInstances/Hero/WarriorHeroAnimInstance.h"
+
+UWarriorHeroAnimInstance* UWarriorHeroLinkedAnimLayer::GetHeroAnimInstance() const
+{
+	// GetOwningComponent는 현재 AnimInstance가 재생중인 SkeletalMeshComponent를 반환합니다.
+	return Cast<UWarriorHeroAnimInstance>(GetOwningComponent()->GetAnimInstance());
+}
