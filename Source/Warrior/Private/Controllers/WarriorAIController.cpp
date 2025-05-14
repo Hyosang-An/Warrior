fb2df7f8b461b10a3d8d312933c9dcd6509206f0
Hyosang_An <gytharj@gmail.com>
2025-05-15 01:26:15 +0900

    Adds WarriorAIController class and integrates CrowdFollowingComponent

---
+++ Source/Warrior/Private/Controllers/WarriorAIController.cpp
@@ -0,0 +1,13 @@
+#include "Controllers/WarriorAIController.h"
+
+#include "WarriorDebugHelper.h"
+#include "Navigation/CrowdFollowingComponent.h"
+
+AWarriorAIController::AWarriorAIController(const FObjectInitializer& ObjectInitializer)
+	:Super(ObjectInitializer.SetDefaultSubobjectClass<UCrowdFollowingComponent>("PathFollowingComponent"))
+{
+	if (UCrowdFollowingComponent* CrowdComp = Cast<UCrowdFollowingComponent>(GetPathFollowingComponent()))
+	{
+		Debug::Print(TEXT("CrowdFollowingComponent valid"), FColor::Green);
+	}
+}
